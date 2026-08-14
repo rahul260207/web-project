38fa93cee53a1531442e9151c01e425daedbff56
DongiriRahul <dongirirahul845548@gmail.com>
2026-08-14 16:42:01 +0530

    last_commiting

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express')
+
 const app = express()
+
+app.use(express.json())
+
 const port = process.env.PORT || 8080
-const users=[
-	{"id":1,
-	"name":"Juan Price",
-	 "gender":"male",
-	"image":"https://randomuser.me/api/portraits/men/59.jpg"},
-	{"id":2,
-	"name":"Dileep Holla",
-	 "gender":"male",
-	"image":"https://randomuser.me/api/portraits/men/60.jpg"},
-	{"id":3,
-	"name":"Harcourt Olivier",
-	 "gender":"male",
-	"image":"https://randomuser.me/api/portraits/men/61.jpg"}
+
+const users = [
+ {
+   "id" : 1,
+   "name" : "Alejandra Romero",
+   "gender" : "male",
+   "image" : "https://randomuser.me/api/portraits/women/62.jpg"
+ },
+ {
+   "id" : 2,
+   "name" : "Olivia Morris",
+   "gender" : "female",
+   "image" : "https://randomuser.me/api/portraits/women/31.jpg"
+ }
 ]
 
+// api server
+
+// get all users
 app.get("/api/users", function(req, res){
  res.status(200).json(users);
 })
@@ -29,23 +37,6 @@ function getUserById(uid){
  return -1;
 }
 
-
-
-// get user by id
-app.get("/api/users/:id", function(req, res)
-{
- var uid = req.params.id;
- var userid = getUserById(uid);
-
-
- if(userid == -1)
- {
-   res.status(404).json({"message" : "user not found"})
- }
- res.status(200).json(users[userid])
-})
-
-
 // get user by id
 app.get("/api/users/:id", function(req, res)
 {
@@ -58,26 +49,60 @@ app.get("/api/users/:id", function(req, res)
  }
  res.status(200).json(users[userid])
 })
-app.get("/api/randomuser", function(req, res)
-{
-	var n= users.length;
-	var randomid = Math.floor(Math.random() * n);
-	res.status(200).json(users[randomid])
+
+// get random user
+app.get("/api/randomuser", function(req, res){
+ var n = users.length;
+ const randomid = Math.floor(Math.random() * n);
+ res.status(200).json(users[randomid])
 })
+
+var newuserid = users.length + 1;
+
+// post: add a new user
 app.post("/api/users", function(req, res)
 {
-	let user = req.body;
-	user.id=newuserid;
-	newuserid++;
-	users.push(user);
-	res.status(201).json({"message" : "user created successfully"})
+ if(!req.body.name || !req.body.gender || !req.body.image)
+   return res.json({"message" : " name, gender and image is required"})
+ let user = req.body;
+ user.id = newuserid;
+ newuserid++;
+ users.push(user)
+ res.status(200).json({"message" : "added successfully"});
 })
 
+// put: update user details of given id
+app.put("/api/users/:id", function(req, res){
+ var userid = getUserById(req.params.id);
 
+ if(userid == -1)
+   return res.json({"message" : "user not found"})
 
+   if(req.body.name)
+     users[userid].name = req.body.name;
 
+   if(req.body.gender)
+     users[userid].gender = req.body.gender;
 
-app.use(express.static('frontend'))
-app.listen(port, function(){
-	console.log("my app is running at http://localhost:" + port)
+   if(req.body.image)
+     users[userid].image = req.body.image;
+
+   return res.status(200).json({"message" : "user details updated", "user" : users[userid]})
+})
+
+app.delete("/api/users/:id", function(req, res){
+ var userid = getUserById(req.params.id);
+ if(userid == -1)
+   return res.json({"message" : "user not found"})
+
+ users.splice(userid, 1);
+
+ res.status(200).json({"message" : "user deleted successfully"})
+
+})
+
+app.use(express.static("frontend")) // web server
+
+app.listen(port, function (){
+ console.log("my app is running at http://localhost:"+port)
 })
